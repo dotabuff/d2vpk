e86673e5343365a50cb8f1b31e3612a695bde13c
Michael Fellinger <m.fellinger@gmail.com>
2013-08-31 01:35:30 +0200

    exclude some more data

--- sync.rb
+++ sync.rb
@@ -6,8 +6,8 @@ Dir.glob("#{dota}/**/*_dir.vpk") do |vpk|
   target = File.basename(vpk.sub(dota+"/", '').tr('/', '_'), '_dir.vpk')
   next if target == "dota_sound_vo_english"
 
-  system('./d2vpk', vpk, target)
-  Dir.glob(target+"/**/*.{wav,vtf,mp3,webm}") do |rm|
+  # system('./d2vpk', vpk, target)
+  Dir.glob(target+"/**/*.{wav,vtf,mp3,webm,mdl,dds,usm,vvd}") do |rm|
     File.unlink(rm)
   end
 end
