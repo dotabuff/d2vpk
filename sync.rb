051efbb4677d0adfa48ef00cc61555453071c8db
Michael Fellinger <m.fellinger@gmail.com>
2013-10-26 20:37:20 +0200

    Couple of fixes for sync

--- sync.rb
+++ sync.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+require 'rake'
 
 dota = "#{ENV["HOME"]}/.steam/steam/SteamApps/common/dota\ 2\ beta"
 ignore = %w[
@@ -8,11 +9,27 @@ ignore = %w[
   resource/flash3/images/hud_skins
 ]
 
+sh 'mkdir', '-p', 'dota/resource'
+sh 'cp', '-r', "#{dota}/dota/resource/", 'dota/'
+Dir.glob('dota/resource/dota_*.txt') do |f|
+  sh 'file', f
+  sh 'iconv', '--verbose', '-f', 'utf-16', '-t', 'utf-8', '-o', "#{f}.utf8", f
+  sh 'mv', "#{f}.utf8", f
+  sh 'dos2unix', f
+end
+
+sh 'git', 'add', 'dota'
+sh 'git', 'add', '-u', 'dota'
+
 Dir.glob("#{dota}/**/*_dir.vpk") do |vpk|
   target = File.basename(vpk.sub(dota+"/", '').tr('/', '_'), '_dir.vpk')
   next if target =~ /dota_sound_vo_english|dota_scaleform_cache/
 
-  system('./d2vpk', vpk, target, *ignore)
-  system('git', 'add', target)
-  system('git', 'commit', '-v')
+  sh './d2vpk', vpk, target, *ignore
+
+  Dir.glob("#{target}/**/*.txt"){|f| sh 'dos2unix', f }
+
+  sh 'git', 'add', target
 end
+
+system('git', 'commit', '-v')
