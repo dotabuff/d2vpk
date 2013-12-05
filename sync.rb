50f275537573f63ef50e2f06e0104d2b7a9a6368
Michael Fellinger <m.fellinger@gmail.com>
2013-12-05 18:25:29 +0100

    extract a bit more, but ignore in git

--- sync.rb
+++ sync.rb
@@ -1,21 +1,23 @@
 #!/usr/bin/env ruby
 require 'rake'
 
+# dota = "#{ENV["HOME"]}/.steam/steam/SteamApps/common/dota\ 2\ test"
 dota = "#{ENV["HOME"]}/.steam/steam/SteamApps/common/dota\ 2\ beta"
-ignore = %w[
-  .wav .vtf .mp3 .webm .mdl .dds .usm .vvd
-  resource/flash3/images/econ
-  resource/flash3/images/sets
-  resource/flash3/images/hud_skins
-]
 
 sh 'mkdir', '-p', 'dota/resource'
 sh 'cp', '-r', "#{dota}/dota/resource/", 'dota/'
-Dir.glob('dota/resource/dota_*.txt') do |f|
-  sh 'file', f
-  sh 'iconv', '--verbose', '-f', 'utf-16', '-t', 'utf-8', '-o', "#{f}.utf8", f
-  sh 'mv', "#{f}.utf8", f
-  sh 'dos2unix', f
+
+Dir.glob('dota/resource/*.txt') do |txt|
+  file = `file "#{txt}"`
+  if file =~ /UTF-16 Unicode text/
+    sh 'iconv', '--verbose', '-f', 'utf-16', '-t', 'utf-8', '-o', "#{txt}.utf8", txt
+    sh 'mv', "#{txt}.utf8", txt
+  else
+    p file
+  end
+  if file =~ /with CRLF(, CR)? line terminators/
+    sh 'dos2unix', '-q', txt
+  end
 end
 
 sh 'git', 'add', 'dota'
@@ -25,9 +27,9 @@ Dir.glob("#{dota}/**/*_dir.vpk") do |vpk|
   target = File.basename(vpk.sub(dota+"/", '').tr('/', '_'), '_dir.vpk')
   next if target =~ /dota_sound_vo_english|dota_scaleform_cache/
 
-  sh './d2vpk', vpk, target, *ignore
+  sh './d2vpk', vpk, target
 
-  Dir.glob("#{target}/**/*.txt"){|f| sh 'dos2unix', f }
+  Dir.glob("#{target}/**/*.{vdf,res,txt}"){|f| sh 'dos2unix', '-q', f }
 
   sh 'git', 'add', target
 end
