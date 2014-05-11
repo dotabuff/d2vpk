c01721f960df8494cd9239c0ce94e25476c47b2c
Michael Fellinger <m.fellinger@gmail.com>
2014-05-11 22:04:20 +0200

    Convert items_game to json

--- convert.rb
+++ convert.rb
@@ -1,26 +1,36 @@
 #!/usr/bin/env ruby
+
 require 'rake'
 require "json"
+require 'fileutils'
 
 require_relative "./vdf"
 
-sh 'mkdir', '-p', 'json/dota/resource'
+%w[
+  dota/resource/items_*.txt
+  dota_pak01/scripts/items/items_game.txt
+].each do |glob|
+  Dir.glob glob do |source|
+    p source
+    json_name = File.basename(source, '.txt') + ".json"
+    directory = File.dirname(source)
+    dest_directory = File.join('json', directory)
+    dest = File.join(dest_directory, json_name)
 
-Dir.glob('dota/resource/items_*.txt') do |source|
-  dest = "json/dota/resource/#{File.basename(source.gsub("txt", "json"))}"
+    FileUtils.mkdir_p dest_directory
 
-  puts "Converting #{source} to #{dest}..."
+    puts "Converting #{source} to #{dest}..."
 
-  begin
-    txt = File.read(source)
-    vdf = VDF.new(txt)
-    res = JSON.pretty_generate(vdf.kvs)
+    begin
+      txt = File.read(source)
+      vdf = VDF.new(txt)
+      res = JSON.pretty_generate(vdf.kvs)
 
-    File.open(dest, "w") {|f| f.write(res) }
-  rescue => ex
-    puts "Error: #{ex}"
+      File.open(dest, "w") {|f| f.write(res) }
+    rescue => ex
+      puts "Error: #{ex}"
+    end
+
+    sh 'git', 'add', dest
   end
-
 end
-
-sh 'git', 'add', 'json/dota/resource/*.json'
